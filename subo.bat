f4899ed1b577cd45a14fb24e66e3ffddd3dcaffe
Ulises <ulimxtz@gmail.com>
2024-06-19 21:56:32 -0600

    AWS Prueba2

--- subo.bat
+++ subo.bat
@@ -3,9 +3,9 @@ git commit -m "AWS Prueba2"
 git push -u origin main
 
 
-set GOOS=linux
-set GOARCH=amd64
- 
-go build -o main main.go
+set GOARCH=amd64 
+set GOOS=linux 
+
+go build -o bootstrap main.go
 del main.zip 
 tar.exe -a -cf main.zip main
