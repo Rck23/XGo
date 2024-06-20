50f4c693ceff23d156d130d4e5a59f507023c7a1
Ulises <ulimxtz@gmail.com>
2024-06-19 18:50:13 -0600

    AWS Prueba2

--- subo.bat
+++ subo.bat
@@ -1,10 +1,11 @@
 git add .
-git commit -m "AWS Prueba1"
+git commit -m "AWS Prueba2"
 git push -u origin main
+
 
 set GOOS=linux
 set GOARCH=amd64
-
-go build main.go
+ 
+go build -o main main.go
 del main.zip 
 tar.exe -a -cf main.zip main
