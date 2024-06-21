344f0071b3984a3d8d5eab6dec62af017c0bdba5
Ulises <ulimxtz@gmail.com>
2024-06-20 21:51:17 -0600

    AWS Prueba3

--- subo.bat
+++ subo.bat
@@ -1,11 +1,13 @@
 git add .
-git commit -m "AWS Prueba2"
+git commit -m "AWS Prueba3"
 git push -u origin main
 
 
-set GOARCH=amd64 
-set GOOS=linux 
+set GOOS=linux
+set GOARCH=amd64
+set CGO_ENABLED=0
+go build -tags lambda.norpc -o bootstrap main.go
+ulimx\Go\bin\build-lambda-zip.exe -o main.zip bootstrap
 
-go build -o bootstrap main.go
 del main.zip 
 tar.exe -a -cf main.zip main
